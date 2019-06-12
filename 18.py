dd92ef351cd6a77cc9902bd43437ce8a109d50d2
dhivya2nandha <50711793+dhivya2nandha@users.noreply.github.com>
2019-06-12 09:47:18 +0530

    Create 18.py

---
+++ 18.py
@@ -0,0 +1,10 @@
+s,e=map(int,input().split())
+for num in range(s,e):
+  temp=num
+  sum=0
+  while temp>0:
+      digit=temp%10
+      sum=sum+digit**3
+      temp=temp//10
+      if sum==num:
+           print (num)
